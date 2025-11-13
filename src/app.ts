d208dfe5c488f1776bfcb8e61cfa16eb9dc84a26
Arshpreet Kaur <arshpreetb417@gmail.com>
2025-11-12 19:50:31 -0600

    Register post CRUD routes in main app.

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+/**
+ * @file app.ts
+ * @description Initializes the Connectify API backend with Express and TypeScript.
+ * This file configures global middleware, a sample root route, and exports the app instance.
+ */
+
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import postsRoutes from "./api/v1/routes/posts.routes"
+
+/**
+ * @constant {Application} app - Main Express application instance.
+ */
+const app: Application = express();
+
+/** Apply security and parsing middleware */
+app.use(express.json()); 
+app.use(cors()); 
+app.use(helmet()); 
+app.use("/api/posts", postsRoutes);
+
+
+/**
+ * @route GET /
+ * @description Root route to confirm the API is working
+ * @returns {string} A welcome message
+ */
+app.get("/", (req: Request, res: Response): void => {
+  res.status(200).send("Welcome to Connectify API!");
+});
+
+export default app;
